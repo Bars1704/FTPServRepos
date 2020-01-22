3273e7b579c1d1ea984ee68f1c31a29cb8682df9
Maks <maksterekhov1704@gmail.com>
2020-01-22 16:13:24 +0200

    Model Changed

---
+++ Models/File.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_ServerTest01
+{
+    class File : PseudoFile
+    {
+        public string Extension { get; set; }
+        public float Size { get; set; }
+    }
+}
---
+++ Models/Folder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_ServerTest01
+{
+    class Folder:PseudoFile
+    {
+        public List<PseudoFile> Files { get; set; }
+    }
+}
